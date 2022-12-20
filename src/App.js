39695236ca954d96b48191776c1ed3bcc00716c1
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-12-20 13:50:33 -0800

    Revert "Add the quotation icon + make the texts transparent to the screen color"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import QuoteBox from './Components/QuoteBox.js'
 
 function App() {
   return (
-    <div  className="bg-yellow-800 h-screen flex flex-col items-center justify-center text-white text-xs" id="quote-box">
+    <div className="App">
       <QuoteBox />
     </div>
   );
