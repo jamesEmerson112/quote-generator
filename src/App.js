b77c01473b9e11394126557109daac6030721de8
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-12-21 00:56:31 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import QuoteBox from './Components/QuoteBox.js'
+import React, { useState, useEffect } from 'react';
+import QuoteBox from './Components/QuoteBox.js';
 
 function App() {
+  const [bgColor, setBgColor] = useState('bg-cyan-900');
+
   return (
     <div  className="bg-cyan-900 h-screen flex flex-col items-center justify-center text-white text-xs" id="quote-box">
       <QuoteBox />
