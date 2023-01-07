10ef4df222ce27e900e239f7419ad38767821e49
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2023-01-06 18:55:54 -0800

    Made some progress with testing

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -13,12 +13,17 @@ const link = 'https://api.api-ninjas.com/v1/quotes?category=';
 function QuoteBox(props) {
   const [quote, setQuote] = useState('Experience is the teacher of all things.');
   const [author, setAuthor] = useState('- Julis Caesar');
+  const [isToggled, setIsToggled] = useState(false);
+
   const changeBgColor = props.changeBgColor;
 
   const updateQuote = async (keyword) => {
     const data = await getQuote(link, keyword);
     setQuote(data['quote']);
     setAuthor('- ' + data['author']);
+
+    setIsToggled(isToggled => !isToggled);
+    console.log(isToggled);
     changeBgColor();
   }
 
@@ -39,8 +44,12 @@ function QuoteBox(props) {
   return (
     <div className="transform-scale-90 relative quote-box" id="quoteBox">
       <div className="flex text-xl">
+      {/* <div className="flex text-xl"> */}
+
         <FontAwesomeIcon icon={faQuoteLeft} className="" />
-        <div id="quote-text" className="font-sans font-bold leading-tight">
+        <div id="quote-text" className={"font-sans font-bold leading-tight " + (isToggled ? "text-sm" : "")}>
+        {/* <div id="quote-text" className={"test"}> */}
+
           <h3 id="text" className="grid justify-items-center" >{quote}</h3>
         </div>
       </div>
